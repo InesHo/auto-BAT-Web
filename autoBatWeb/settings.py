281e1f58f5af90e4911b447a003c101312130ed6
wail-abbas <wailabbas.ahmed@gmail.com>
2022-12-16 10:34:07 +0100

    finalizing production settings

--- autoBatWeb/settings.py
+++ autoBatWeb/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'populateDB',
     'background_task',
+    'mod_wsgi.server',
 
 ]
 
--- autoBatWeb/wsgi.py
+++ autoBatWeb/wsgi.py
@@ -7,23 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
-
-# =====================
-# wsgi.py file begin 
-
-import os, sys
-# add the hellodjango project path into the sys.path
-sys.path.append('/home/abusr/autoBatWeb/auto-BAT-Web')
-
-# add the virtualenv site-packages path to the sys.path
-sys.path.append('/home/abusr/anaconda3/envs/AutoBat/lib/python3.8/site-packages')
-
-# poiting to the project settings
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'autoBatWeb.settings')
+import os
 
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'autoBatWeb.settings')
+
 application = get_wsgi_application()
-
-# wsgi.py file end
-# ===================
-
